64c839555ad91549614bb9b3093cb614b3a29469
vladyslav4ik <vpoltavets02@gmail.com>
2023-05-04 20:25:26 +0300

    Little fixes

--- src/main/java/coworking/project/CoworkingProjectApplication.java
+++ src/main/java/coworking/project/CoworkingProjectApplication.java
@@ -1,7 +1,9 @@
 package coworking.project;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CoworkingProjectApplication {
@@ -10,4 +12,9 @@ public class CoworkingProjectApplication {
         SpringApplication.run(CoworkingProjectApplication.class, args);
     }
 
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
 }
--- src/main/java/coworking/project/models/Person.java
+++ src/main/java/coworking/project/models/Person.java
@@ -4,39 +4,36 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Entity
-@Table(name = "person")
+@Table(name = "PERSON")
 @NoArgsConstructor
 @Data
 public class Person {
     @Id
-    @Column(name = "person_id")
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "email")
-    @Pattern(regexp = "[a-z0-9.]{5,}+@gmail.com", message = "Ваша почтова адреса повинна відповідати патерну: " +
-            " тільки літери a-z, цифри 0-9 та . + @gmail.com")
+    @Email
+    @NotEmpty
+    @Column(name = "email", unique = true)
     private String email;
 
-    @NotEmpty(message = "Ім`я користувача не може бути порожнім")
-    @Size(min = 2, max = 100, message = "Довжина імені повинна бути у проміжку від 2 до 100 символів")
-    @Column(name = "username")
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    @Column(name = "username", unique = true)
     private String username;
 
+    @Size(min = 8)
     @Column(name = "password")
-    //@Size(min = 8, max = 16, message = "Довжина паролю повинна бути у проміжку від 8 до 16 символів")
     private String password;
 
     @Column(name = "role")
     private String role;
 
+    @Min(value = 1915)
     @Column(name = "year_of_birth")
-    @Min(value = 1915, message = "Рік народження повинен бути більшим за 1915")
     private Integer yearOfBirth;
 }
